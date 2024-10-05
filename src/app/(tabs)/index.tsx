ad8cc5bc49320e2f46b4702fedb3db0439cce09b
Codebox124 <rajcodes733@gmail.com>
2024-10-05 20:53:53 +0100

    created post Ui

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,12 @@
 import { FlatList } from "react-native";
 import posts from '@/assets/data/posts.json'
 import PostListItem from "@/src/components/PostListItem";
-
 export default function HomeScreen() {
     return (
         <FlatList
             data={posts}
             contentContainerStyle={{ gap: 10 }}
             renderItem={({ item }) => <PostListItem post={item}
-
             />}
             showsVerticalScrollIndicator={false}
         />
--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,11 +1,31 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, Image, TextInput, Pressable } from "react-native";
+import { useState } from "react";
 
-export default function NewScreen() {
+export default function CreatePost() {
+
+    const [caption, setCaption] = useState('')
     return (
-        <View>
-            <Text>About Me</Text>
-            <Link href='/' >Feeds</Link>
+        <View className="p-3 items-center flex-1">
+            <Image className="w-52 aspect-[3/4] rounded-lg " source={{ uri: 'https://pbs.twimg.com/profile_images/1842539587786199040/DRiMD9wv_400x400.jpg' }} />
+
+            <Text onPress={() => { }}
+                className="text-blue-500 font-semibold m-5">Change</Text>
+
+            <TextInput value={caption}
+                onChangeText={(newValue) => setCaption(newValue)}
+                placeholder="Wha is on your mind "
+                className=" w-full p-3"
+            />
+            <View className="mt-auto w-full">
+                <Pressable className="bg-blue-500  w-full p-3 items-center rounded-md  ">
+                    <Text className="text-white font-semibold">Share</Text>
+                </Pressable>
+
+            </View>
+
+
+
+
 
         </View>
     );
